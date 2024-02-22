bcd3585c2f7ac6393c3a9bb37f0270941819b942
Priya Singh <priyasingh42613@gmail.com>
2024-02-22 12:30:36 +0530

    DSA

---
+++ Binary_Sort.java
@@ -0,0 +1,23 @@
+public class Binary_Sort {
+    private static boolean isPresent(int arr[], int n)
+    {
+        int low = 0, high = arr.length;
+        int mid;
+        while(low <= high)
+        {
+            mid = (low + high) / 2;
+            if(arr[mid] == n) return true;
+            else if(arr[mid] < n)
+                low = mid + 1;
+            else 
+                high = mid - 1;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+        int n = 3;
+        int arr[]= {1, 2, 3, 4, 5};
+        System.out.println(isPresent(arr, n));
+    }
+}
